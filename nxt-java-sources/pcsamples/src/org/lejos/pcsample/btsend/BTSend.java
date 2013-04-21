7687ffa8207b541c72a52b9ba0fdacf2e4ae0450
Mihai Maruseac <mihai@rosedu.org>
2013-04-21 08:52:19 +0300

    Clenaup sample sources.

--- nxt-java-sources/pcsamples/src/org/lejos/pcsample/btsend/BTSend.java
+++ nxt-java-sources/pcsamples/src/org/lejos/pcsample/btsend/BTSend.java
@@ -11,60 +11,55 @@ import lejos.util.Delay;
 /**
  * This is a PC sample. It connects to the NXT, and then
  * sends an integer and waits for a reply, 100 times.
- * 
- * Compile this program with javac (not nxjc), and run it 
+ *
+ * Compile this program with javac (not nxjc), and run it
  * with java.
- * 
- * You need pccomm.jar and bluecove.jar on the CLASSPATH. 
+ *
+ * You need pccomm.jar and bluecove.jar on the CLASSPATH.
  * On Linux, you will also need bluecove-gpl.jar on the CLASSPATH.
- * 
+ *
  * Run the program by:
- * 
- *   java BTSend 
- * 
+ *
+ *   java BTSend
+ *
  * Your NXT should be running a sample such as BTReceive or
  * SignalTest. Run the NXT program first until it is
- * waiting for a connection, and then run the PC program. 
- * 
- * @author Lawrie Griffiths
+ * waiting for a connection, and then run the PC program.
  *
+ * @author Lawrie Griffiths
  */
-public class BTSend {	
+public class BTSend {
 	public static void main(String[] args) {
-		
-		NXTConnector conn = new NXTConnector();
-	
-		conn.addLogListener(new NXTCommLogListener(){
 
+		NXTConnector conn = new NXTConnector();
+		conn.addLogListener(new NXTCommLogListener(){
 			public void logEvent(String message) {
 				System.out.println("BTSend Log.listener: "+message);
-				
 			}
 
 			public void logEvent(Throwable throwable) {
 				System.out.println("BTSend Log.listener - stack trace: ");
-				 throwable.printStackTrace();	
+				throwable.printStackTrace();
 			}
-		} 
-		);
-		
+		});
+
 		// Connect to any NXT over Bluetooth
 		boolean connected = conn.connectTo("btspp://");
-	
+
 		if (!connected) {
 			System.err.println("Failed to connect to any NXT");
 			System.exit(1);
 		}
-		
+
 		DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
 		Random rand = new Random();
-				
+
 		try{
 			int n = 9;
-			
+
 			int [] x = new int [n];
 			int [] y = new int [n];
-			
+
 			x[0] = 0; y[0] = 0;
 			x[1] = 1; y[1] = 0;
 			x[2] = 2; y[2] = 1;
@@ -74,19 +69,14 @@ public class BTSend {
 			x[6] = 1; y[6] = 7;
 			x[7] = -2; y[7] = 6;
 			x[8] = -3; y[8] = 3;
-			
+
 			Curve c = new Curve(n, x, y);
-			c.write(dos);		
+			c.write(dos);
 			System.out.println(c.ToString());
 			dos.flush();
-			
-		} catch (Exception e) 
-		{
+		} catch (Exception e)
 			e.printStackTrace();
-		}
-		
-		//Delay.msDelay(5000);
-		
+
 		try {
 			dos.close();
 			conn.close();
--- nxt-java-sources/pcsamples/src/org/lejos/pcsample/btsend/Curve.java
+++ nxt-java-sources/pcsamples/src/org/lejos/pcsample/btsend/Curve.java
@@ -1,66 +1,54 @@
-
 package org.lejos.pcsample.btsend;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-public class Curve 
+public class Curve
 {
 	public int n;
-	
 	public int [] x;
-	
 	public int [] y;
-	
+
 	public Curve() { }
-	
-	public Curve(int n, int [] x, int [] y)
-	{
+
+	public Curve(int n, int [] x, int [] y) {
 		this.n = n;
 		this.x = new int [n];
 		this.y = new int [n];
-		for (int i=0; i<n; i++)
-		{
+		for (int i=0; i<n; i++) {
 			this.x[i] = x[i];
 			this.y[i] = y[i];
 		}
 	}
-	
-	public int GetXKth(int i, int k)
-	{
+
+	public int GetXKth(int i, int k) {
 		return ((i + k) < n) ? x[i+k] : x[n-1];
 	}
-	
-	public int GetYKth(int i, int k)
-	{
+
+	public int GetYKth(int i, int k) {
 		return ((i + k) < n) ? y[i+k] : y[n-1];
 	}
-	
-	public void read(DataInputStream in) throws IOException
-	{
+
+	public void read(DataInputStream in) throws IOException {
 		this.n = in.readInt();
 		this.x = new int [n];
 		this.y = new int [n];
-		for (int i=0; i<n; i++)
-		{
+		for (int i=0; i<n; i++) {
 			this.x[i] = in.readInt();
 			this.y[i] = in.readInt();
 		}
 	}
-	
-	public void write(DataOutputStream out) throws IOException
-	{
+
+	public void write(DataOutputStream out) throws IOException {
 		out.writeInt(n);
-		for (int i=0; i<n; i++)
-		{
+		for (int i=0; i<n; i++) {
 			out.writeInt(x[i]);
 			out.writeInt(y[i]);
 		}
 	}
-	
-	public String ToString()
-	{
+
+	public String ToString() {
 		String s = n + ":";
 		for (int i=0; i<n; i++)
 			s += "(" + x[i] + "," + y[i] + ") ";
